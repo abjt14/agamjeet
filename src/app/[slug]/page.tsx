96366c99d1fdd80db6a8b43481d4440c0a4d5d84
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-06-19 03:42:56 -0700

    added a new spoiler alert to mock test articles

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -48,20 +48,6 @@ export default function Article({ params } : { params: { slug: string } }) {
     <section className="flex flex-col items-start justify-between gap-8 w-full">
       <div className="flex flex-col gap-4 sm:gap-2">
         <h1 className="text-cinder-800 font-medium dark:font-normal text-4xl dark:text-macaroni-and-cheese-300 font-ebgaramond">{article.title}</h1>
-        {
-          article.category === 'mock-test' && (
-            <div className="flex gap-2 justify-between sm:justify-start">
-              <DownloadButton
-                type="problems"
-                slug={article.slug}
-              />
-              <DownloadButton
-                type="answer-key"
-                slug={article.slug}
-              />
-            </div>
-          )
-        }
         <div className="flex gap-2 justify-start items-baseline text-sm text-cinder-800 dark:text-cinder-500">
           <span>{format(new Date(article.publishedAt), 'd MMMM yyyy')}</span>
           <span>•</span>
@@ -70,7 +56,30 @@ export default function Article({ params } : { params: { slug: string } }) {
         </div>
       </div>
       {
-        article.category === 'mock-test' && <span className="text-cinder-800 dark:text-macaroni-and-cheese-300">This article contains spoilers for the mock test.</span>
+        article.category === 'mock-test' && (
+          <div className="flex flex-col gap-4 p-4 rounded-md border border-macaroni-and-cheese-300 bg-macaroni-and-cheese-50 dark:bg-cinder-950">
+            <h2 className="flex gap-2 justify-start items-center text-cinder-800 dark:text-macaroni-and-cheese-300 font-medium dark:font-normal">
+              This article contains spoilers for the mock test.
+            </h2>
+            <div className="grid gap-2 text-macaroni-and-cheese-800 dark:text-cinder-300">
+              <p className="">I recommend attempting the problems before proceeding to read the article. You can download the problems and the answer key using the buttons below.</p>
+            </div>
+            <div className="flex gap-2 justify-between sm:justify-start pt-2">
+              <DownloadButton
+                type="problems"
+                slug={article.slug}
+              />
+              {
+                article.answerKey && (
+                  <DownloadButton
+                    type="answer-key"
+                    slug={article.slug}
+                  />
+                )
+              }
+            </div>
+          </div>
+        )
       }
       <MDX code={article.body.code} />
     </section>
