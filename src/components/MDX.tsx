66b3f9e43c95ef9fbd33e89b6fac5622e9938daf
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-06-22 20:49:38 -0700

    added custom link component to mdx with target blank

--- src/components/MDX.tsx
+++ src/components/MDX.tsx
@@ -1,15 +1,20 @@
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import type { MDXComponents } from 'mdx/types';
 
 interface MDXProps {
   code: string;
 }
 
+const mdxComponents: MDXComponents = {
+  a: ({ href, children }) => <a href={href as string} target="_blank" className="underline underline-offset-4 decoration-macaroni-and-cheese-400 dark:decoration-cinder-600 hover:decoration-macaroni-and-cheese-800 hover:dark:decoration-cinder-300 duration-150 transition-[text-decoration-color] will-change-auto">{children}</a>,
+}
+
 export default function MDX({ code }: MDXProps) {
-  const MDXComponent = useMDXComponent(code);
+  const MDXContent = useMDXComponent(code);
 
   return (
     <article className="flex flex-col gap-4">
-      <MDXComponent />
+      <MDXContent components={mdxComponents} />
     </article>
   )
 }
