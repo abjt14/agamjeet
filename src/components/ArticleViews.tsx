5432f61e26d69bc1453757c75614ec056ef4f8af
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-07-02 17:12:59 -0700

    added loader animation for views shown in home page and article pages

--- src/components/ArticleViews.tsx
+++ src/components/ArticleViews.tsx
@@ -24,13 +24,19 @@ export default function ArticleViews({ slug, trackView }: {
   }, [slug, trackView]);
 
   return (
-    isLoading ? (
-      <></>
-    ) : (
-      <>
-        <span>&#10022;</span>
-        <span>{views} views</span>
-      </>
-    )
+    <>
+      <span>&#10022;</span>
+      <span className="flex items-center gap-1">
+        {
+          isLoading ? (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-3 h-3 animate-spin">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+          ) : (
+            <span>{views}</span>
+          )
+        }
+      views</span>
+    </>
   )
 }
